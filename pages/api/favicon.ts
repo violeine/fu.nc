20cf4d41bddfc48b96bba607b09c3a654f85d3d1
violeine <haluudang@gmail.com>
2022-04-02 23:51:52 +0700

    feat: use vercel edge caching

--- pages/api/favicon.ts
+++ pages/api/favicon.ts
@@ -1,29 +1,12 @@
-import LRUCache from "lru-cache";
 import type { NextApiRequest, NextApiResponse } from "next";
-import cache from "../../utils/cache";
 import { favicon } from "../../utils/favicon";
 
-const send = (res: NextApiResponse, img: Buffer) => {
-  res.setHeader("Content-Type", "image/png");
-  res.send(img);
-};
-async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse,
-  lru: LRUCache<string, Buffer>
-) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { user, size } = req.query;
-  console.log(lru, user, size);
-  if (lru.has(user as string)) {
-    console.log("loaded from cache");
-    const imageData = lru.get(user as string);
-    imageData && send(res, imageData);
-  } else {
-    const imageData = await favicon(user as string, size ? Number(size) : 64);
-    console.log("save into cache");
-    lru.set(user as string, imageData);
-    send(res, imageData);
-  }
+  const imageData = await favicon(user as string, size ? Number(size) : 64);
+  res.setHeader("Cache-Control", `max-age=0,s-maxage=${24 * 60 * 60}`);
+  res.setHeader("Content-Type", "image/png");
+  res.status(200).send(imageData);
 }
 
-export default cache(handler);
+export default handler;
